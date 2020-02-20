1afa509ee05823cc50621979e8e323a73b47dabe
Doak Covington <doakcovington@protonmail.com>
2020-02-20 02:58:19 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,1 +1,12 @@
 #SUPER class 
+
+class User
+
+  attr_accessor :first_name, :last_name
+
+  def initialize(first_name, last_name)
+    @first_name = first_name
+    @last_name = last_name
+  end
+
+end
